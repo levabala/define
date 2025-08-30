9a1f3cd0a1f14d57fe5032ab49f18af64ae122a1
levabala <levabalaanother@gmail.com>
2025-08-30 15:18:41 +0300

    add typescript error suppressions for vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+// @ts-expect-error
 import viteFastify from '@fastify/vite/plugin';
+// @ts-expect-error
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
